d693e85f34f82bcc821ef8ba823ac845edf945dd
James Devlin <j@dvln.dev>
2019-12-25 23:33:30 -0800

    added ARG for GEOS PostGIS PLV8

--- rootfs/postunpack.sh
+++ rootfs/postunpack.sh
@@ -6,35 +6,6 @@
 . /libfs.sh
 . /libpostgresql.sh
 
-# Install PostGIS tooling
-cd ~
-wget http://download.osgeo.org/geos/geos-3.7.1.tar.bz2
-tar xvf geos-3.7.1.tar.bz2
-cd geos-3.7.1
-./configure
-make -j $(nproc)
-make install
-# Install postgis-2.5.2
-cd ~
-wget https://download.osgeo.org/postgis/source/postgis-2.5.2.tar.gz
-tar xzvf postgis-2.5.2.tar.gz
-cd postgis-2.5.2
-./configure
-make -j  $(nproc)
-make install
-# Install plv8
-cd ~
-wget https://github.com/plv8/plv8/archive/v2.3.9.tar.gz
-tar -xvzf v2.3.9.tar.gz
-cd plv8-2.3.9/
-make -j  $(nproc) 
-make install
-ldconfig
-# Clean up
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-rm -rf ~/geos-3.7.1.tar.bz2 ~/geos-3.7.1 ~/postgis-2.5.2.tar.gz ~/postgis-2.5.2 ~/v2.3.9.tar.gz ~/plv8-2.3.9
-
-
 # Load PostgreSQL environment variables
 eval "$(postgresql_env)"
 
@@ -45,3 +16,32 @@ chmod -R g+rwX "$POSTGRESQL_INITSCRIPTS_DIR" "$POSTGRESQL_TMP_DIR" "$POSTGRESQL_
 
 # Redirect all logging to stdout
 ln -sf /dev/stdout "$POSTGRESQL_LOG_DIR/postgresql.log"
+
+# Install PostGIS tooling - first is GEOS
+cd ~
+wget http://download.osgeo.org/geos/geos-${GEOS_VER}.tar.bz2
+tar xvf geos-${GEOS_VER}.tar.bz2
+cd geos-${GEOS_VER}
+./configure
+make -j $(nproc)
+make install
+# Install postgis-2.5.2
+cd ~
+wget https://download.osgeo.org/postgis/source/postgis-${POST_GIS_VER}.tar.gz
+tar xzvf postgis-${POST_GIS_VER}.tar.gz
+cd postgis-${POST_GIS_VER}
+./configure
+make -j  $(nproc)
+make install
+# Install plv8
+cd ~
+wget https://github.com/plv8/plv8/archive/v${PLV8_VER}.tar.gz
+tar -xvzf v${PLV8_VER}.tar.gz
+cd plv8-${PLV8_VER}/
+make -j  $(nproc) 
+make install
+ldconfig
+# Clean up
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+rm -rf ~/geos-${GEOS_VER}.tar.bz2 ~/geos-${GEOS_VER} ~/postgis-${POST_GIS_VER}.tar.gz ~/postgis-${POST_GIS_VER} ~/v${PLV8_VER}.tar.gz ~/plv8-${PLV8_VER}
+ 
